7d1275655ae23e6fa80ec604e0c0a5a2067f220f
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-09-25 10:40:14 +0530

    Initial commit

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center h-screen p-5">
+     <div className="text-6xl">Dashboard Page</div>
+     <div className="pt-10 text-4xl pb-4">Pages: </div>
+     <div className="flex flex-col justify-start text-2xl text-blue-400">
+      <a href="/calendar">Calendar</a>
+      <a href="/remember">Remember</a>
+      <a href="/settings">Settings</a>
+      <a href="/tasks">Tasks</a>
+     </div>
+    </div>
+  );
+}
