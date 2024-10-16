9484cb4f42033efbf25cab83a1d12ff22cc5c1f7
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-10-16 23:41:20 +0530

    Setup Routing across Application

--- src/app/nav.js
+++ src/app/nav.js
@@ -2,8 +2,10 @@
 
 import { Cog6ToothIcon, CalendarDaysIcon, BellSlashIcon, MapIcon, UserIcon, RocketLaunchIcon, ArrowRightEndOnRectangleIcon, RectangleGroupIcon, RectangleStackIcon, PencilSquareIcon, ClipboardDocumentCheckIcon } from '@heroicons/react/24/outline'
 import React, { useState } from 'react';
+import { useRouter } from 'next/navigation'
 
 export function Nav() {
+    const router = useRouter();
     return (
         <>
             <nav className="bg-white fixed w-screen h-20 flex border-b-2 border-neutral-200">
@@ -17,8 +19,8 @@ export function Nav() {
                     <input type="text" className="rounded-sm w-full h-12 ring-neutral-300 ring-2 pl-4 pr-4 text-lg text-black" placeholder=" 🔍  Search for anything " />
                 </div>
                 <div className="w-[30%] flex justify-evenly items-center">
-                    <button className="rounded-sm w-12 h-12 ring-neutral-300 ring-2 flex justify-center items-center"><Cog6ToothIcon className="size-8 stroke-black" /></button>
-                    <button className="rounded-sm w-12 h-12 ring-neutral-300 ring-2 flex justify-center items-center"><CalendarDaysIcon className="size-8 stroke-black" /></button>
+                    <button onClick={() => router.push('/settings')} className="rounded-sm w-12 h-12 ring-neutral-300 ring-2 flex justify-center items-center"><Cog6ToothIcon className="size-8 stroke-black" /></button>
+                    <button onClick={() => router.push('/calendar')} className="rounded-sm w-12 h-12 ring-neutral-300 ring-2 flex justify-center items-center"><CalendarDaysIcon className="size-8 stroke-black" /></button>
                     <button className="rounded-sm w-12 h-12 ring-neutral-300 ring-2 flex justify-center items-center"><BellSlashIcon className="size-8 stroke-black" /></button>
                     <button className="rounded-sm w-36 h-12 ring-neutral-300 ring-2 flex justify-evenly items-center">
                         <h2 className="text-2xl text-black">Timeline</h2>
@@ -35,7 +37,7 @@ export function Nav() {
 export function SideNav() {
 
     const [open, setOpen] = useState(true);
-
+    const router = useRouter();
     const handleToggle = () => {
         setOpen(!open);
     };
@@ -44,22 +46,22 @@ export function SideNav() {
         <>
             <sidenav className={`fixed bg-white transition-all duration-300 ${open ? 'w-64' : 'w-16 pr-4'}`}>
                 <div className={`border-r-2 border-neutral-200 pt-2 relative flex flex-col justify-start space-y-4 items-center h-[92svh] pl-4 transition-all duration-300 ${open ? 'w-64' : 'w-16 pr-4'}`}>
-                    <button className="h-12 w-64 flex justify-evenly items-center text-2xl text-black">
+                    <button className="h-12 w-64 flex justify-evenly items-center text-2xl text-black" onClick={() => router.push('/')}>
                         <h2 className={`w-36 text-start transition-all delay-800 ${open ? '' : 'hidden'}`}>Dashboard</h2> <RectangleGroupIcon className="size-8 stroke-black" />
                     </button>
-                    <button className="h-12 w-64 flex justify-evenly items-center text-2xl text-black">
+                    <button className="h-12 w-64 flex justify-evenly items-center text-2xl text-black" onClick={() => router.push('/tasks')}>
                         <h2 className={`w-36 text-start transition-all duration-300 ${open ? '' : 'hidden'}`}>Tasks</h2> <ClipboardDocumentCheckIcon className="size-8 stroke-black" />
                     </button>
-                    <button className="h-12 w-64 flex justify-evenly items-center text-2xl text-black">
+                    <button className="h-12 w-64 flex justify-evenly items-center text-2xl text-black" onClick={() => router.push('/notes')}>
                         <h2 className={`w-36 text-start transition-all duration-300 ${open ? '' : 'hidden'}`}>Notes</h2> <PencilSquareIcon className="size-8 stroke-black" />
                     </button>
-                    <button className="h-12 w-64  flex justify-evenly items-center text-2xl text-black">
+                    <button className="h-12 w-64  flex justify-evenly items-center text-2xl text-black" onClick={() => router.push('/calendar')}>
                         <h2 className={`w-36 text-start transition-all duration-300 ${open ? '' : 'hidden'}`}>Calendar</h2> <CalendarDaysIcon className="size-8 stroke-black" />
                     </button>
-                    <button className="h-12 w-64 flex justify-evenly items-center text-2xl text-black">
+                    <button className="h-12 w-64 flex justify-evenly items-center text-2xl text-black" onClick={() => router.push('/remember')}>
                         <h2 className={`w-36 text-start transition-all duration-300 ${open ? 'translate-x-0' : 'hidden -translate-x-28'}`}>Remember</h2> <RectangleStackIcon className="size-8 stroke-black" />
                     </button>
-                    <button className="h-12 w-64 flex justify-evenly items-center text-2xl text-black">
+                    <button className="h-12 w-64 flex justify-evenly items-center text-2xl text-black" onClick={() => router.push('/workflow')}>
                         <h2 className={`w-36 text-start transition-all duration-300 ${open ? '' : 'hidden'}`}>Workflow</h2> <RocketLaunchIcon className="size-8 stroke-black" />
                     </button>
                     <button
