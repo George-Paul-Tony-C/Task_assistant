91e159a567cee9f21e6085610d5aa7819dd45965
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-10-26 18:37:16 +0530

    Add Remember page UI

--- src/app/remember/page.jsx
+++ src/app/remember/page.jsx
@@ -1,3 +1,8 @@
+'use client'
+
+import { AnimatePresence, motion } from 'framer-motion';
+
+
 export default function Remember() {
 
   let remember = {
@@ -54,24 +59,35 @@ export default function Remember() {
   };
 
     return (
-      <div className="w-screen min-h-screen p-12">
-        <div className="grid grid-cols-3 grid-flow-dense gap-8 place-items-center items-start">
-  
-          {/* Looping with the provide format to render each note */}
-          {/* This design is a work in progress template */}
-          {/* Weird sizing is intenntional*/}
-          {Object.keys(remember).map((key) => (
-            <div key={key} className="text-black flex w-80 min-h-32 items-center justify-center flex-col border-2 border-black p-5">
-              <h2>{remember[key].name}</h2>
-              <p>{remember[key].date}</p>
-              <p>{remember[key].content}</p>
-            </div>
-          ))}
-  
-          {/* Needs to be offloaded to a secondary function*/}
-  
-        </div>
+      <>
+      <div className="w-screen min-h-screen bg-gray-100 p-12 overflow-auto grid grid-cols-2 gap-4">
+      <div className="flex justify-between items-center mb-2">
+        <h2 className="text-black font-bold text-3xl">Remember Page</h2>
       </div>
+      <p className="max-w-sm text-gray-600 text-md mb-4">
+      </p>
+      <AnimatePresence>
+        {Object.keys(remember).map((key) => (
+          <motion.div
+            key={key}
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -50 }}
+            transition={{ duration: 0.5 }}
+            className="bg-white shadow-md rounded-lg p-5 mb-4"
+          >
+            <div className="flex justify-between items-center mb-2">
+              <h2 className="text-gray-800 font-bold">{remember[key].name}</h2>
+              <p className="text-gray-400 text-xs">Added on {remember[key].date}</p>
+            </div>
+            <p className="max-w-sm text-gray-600 text-md mb-4">
+            {remember[key].content}
+            </p>
+          </motion.div>
+        ))}
+      </AnimatePresence>
+    </div>
+    </>
     );
   }
   
