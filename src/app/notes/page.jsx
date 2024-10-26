1da890b643e7e374d86de50aa323ccb158b2d5b5
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-10-26 18:30:09 +0530

    Update Notes UI

--- src/app/notes/page.jsx
+++ src/app/notes/page.jsx
@@ -1,3 +1,8 @@
+'use client'
+
+import { AnimatePresence, motion } from 'framer-motion';
+
+
 export default function Notes() {
 
     // Temporary data for notes: API will be availible soon.
@@ -58,24 +63,37 @@ export default function Notes() {
   
   
     return (
-      <div className="w-screen min-h-screen p-12 overflow-auto">
-        <div className="grid grid-cols-auto grid-flow-row gap-8 place-items-center items-start">
-  
-          {/* Looping with the provide format to render each note */}
-          {/* This design is a work in progress template */}
-          {/* Weird sizing is intenntional*/}
-          {Object.keys(notes).map((key) => (
-            <div key={key} className="text-black flex w-max min-h-32 items-center justify-center flex-col border-2 border-black p-5">
-              <h2>{notes[key].name}</h2>
-              <p>{notes[key].date}</p>
-              <p>{notes[key].content}</p>
-            </div>
-          ))}
-  
-          {/* Needs to be offloaded to a secondary function*/}
-  
-        </div>
+      <>
+      <div className="w-screen min-h-screen bg-gray-100 p-12 overflow-auto grid grid-cols-2 gap-4">
+      <div className="flex justify-between items-center mb-2">
+        <h2 className="text-black font-bold text-3xl">Notes Page</h2>
       </div>
+      <p className="max-w-sm text-gray-600 text-md mb-4">
+      </p>
+      <AnimatePresence>
+        {Object.keys(notes).map((key) => (
+          <motion.div
+            key={key}
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -50 }}
+            transition={{ duration: 0.5 }}
+            className="bg-white shadow-md rounded-lg p-5 mb-4"
+          >
+            <div className="flex justify-between items-center mb-2">
+              <h2 className="text-gray-800 font-bold">{notes[key].name}</h2>
+              <p className="text-gray-400 text-xs">Added on {notes[key].date}</p>
+            </div>
+            <p className="max-w-sm text-gray-600 text-md mb-4">
+              {notes[key].content}
+            </p>
+          </motion.div>
+        ))}
+      </AnimatePresence>
+    </div>
+    </>
+
+
     );
   }
   
